e0d972493ab41443af9db64f9c45306b6e640e1b
MostafaElGindyy <sasa1382004@gmail.com>
2024-12-10 21:34:23 +0200

    2
    2

--- My project/Assets/Scripts/Explode.cs
+++ My project/Assets/Scripts/Explode.cs
@@ -2,14 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Explode : MonoBehaviour
+public class Brick : MonoBehaviour
 {
+<<<<<<< Updated upstream
     private void OnTriggerEnter(Collider other)
+=======
+    private SpriteRenderer sr;
+    public Sprite explodedBlock;
+
+    void Start()
+>>>>>>> Stashed changes
     {
         if (other.CompareTag("Player"))
         {
+<<<<<<< Updated upstream
             Destroy(gameObject);
             gameObject.SetActive(false); // Makes the platform disappear
         }
+=======
+            if (other.gameObject.tag == "Player" && other.GetContact(0).point.y < transform.position.y)
+            {
+                sr.sprite = explodedBlock;
+                Object.Destroy(gameObject, .2f);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+>>>>>>> Stashed changes
     }
 }
