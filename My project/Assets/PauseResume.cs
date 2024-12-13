4a742d4bae5200b90b97f051e4c837833bb58c38
mohamedwal04 <mohamed230663@bue.edu.eg>
2024-12-13 04:45:25 +0200

    l2
    l2

---
+++ My project/Assets/PauseResume.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseResume : MonoBehaviour
+{
+
+    public GameObject PauseScreen;
+    public static bool paused;
+    public KeyCode PauseButton;
+
+
+    void Start()
+    {
+        paused = false;
+        PauseScreen.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(PauseButton) && ! paused)
+        {
+            Pause();
+        }
+        else if(Input.GetKeyDown(PauseButton) && paused)
+        {
+            Resume();
+        }
+
+    }
+
+    public void Resume()
+    {
+        PauseScreen.SetActive(false);
+        paused = false;
+        Time.timeScale = 1;
+    }
+
+    void Pause()
+    {
+        PauseScreen.SetActive(true);
+        paused = true;
+        Time.timeScale = 0;
+    }
+
+}
+
