f4e77b4a51fb4cd60712a683b6775d15e0f3809a
Ahmed Hossam <ahmedabdelkhalek.7411@gmail.com>
2024-12-16 00:10:18 +0200

    Update Level 2.unity
    level1

--- My project/Assets/LVL1EnemyScene.cs
+++ My project/Assets/LVL1EnemyScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LVL2Enemyscene : MonoBehaviour
+public class LVL1EnemyScene : MonoBehaviour
 {
     public Dialogue dialogueManager;
     private bool dialogueTriggered = false; // Add a flag to track if dialogue has already been triggered
@@ -14,10 +14,12 @@ public class LVL2Enemyscene : MonoBehaviour
             dialogueTriggered = true; // Set the flag to true
             string[] dialogue =
             {
-                "Jack: I�m sorry, Max. I just followed Rico's orders",
-                "Max: I don�t care about your apology. I want to know where is my son located!",
-                "Jack: Vince planned it all. He�s the one you should be after",
-                "Jack: I know the his lab's location. But, I won't tell you unless you defeat me",
+
+    "Now tell me! Where is my son!?",
+    "Leo replies, “I don’t know and I never knew where your son is.",
+     "Leo adds, I never agreed with Rico’s plan to kidnap your son ... but Jack should know where they took your boy.",
+     "defeat me and ill willingly tell you"
+
             };
             dialogueManager.SetSentences(dialogue);
             dialogueManager.StartCoroutine(dialogueManager.TypeDialogue());
