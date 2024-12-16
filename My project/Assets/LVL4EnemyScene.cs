c0956e6d0a6bd99288a22a4379e7f88e92f07322
Ahmed Hossam <ahmedabdelkhalek.7411@gmail.com>
2024-12-16 02:47:44 +0200

    level 4
    level 4

---
+++ My project/Assets/LVL4EnemyScene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LVL4EnemyScene : MonoBehaviour
+{
+    public Dialogue dialogueManager;
+    private bool dialogueTriggered = false; // Add a flag to track if dialogue has already been triggered
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && !dialogueTriggered) // Check if dialogue hasn't been triggered yet
+        {
+            dialogueTriggered = true; // Set the flag to true
+            string[] dialogue =
+            {
+                "After several brutal fights, finally Rico appears.",
+            "Rico : “You were always too soft, Max.”",
+            "Max does not respond and gets ready to fight his last fight",
+            "Overwhelmed with all the enemies he faced, Max gathers all his strength",
+
+    };
+            dialogueManager.SetSentences(dialogue);
+            dialogueManager.StartCoroutine(dialogueManager.TypeDialogue());
+        }
+}
+}
